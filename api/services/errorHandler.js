d043c6414e96351e09b767c3ef43e8aa4b1a22cf
Kyle Cline <kyledcline@gmail.com>
2015-02-10 17:12:59 -0500

    Add errorHandler and Log model

---
+++ api/services/errorHandler.js
@@ -0,0 +1,35 @@
+module.exports = {
+
+  ERRORS: errors(),
+  intercept: intercept
+
+}
+
+function errors () {
+  return {
+    NullCollectionError: {
+      error: 'NullCollectionError',
+      status: 404,
+      message: 'Collection not found.'
+    },
+    InternalServerError: {
+      error: 'InternalServerError',
+      status: 500,
+      message: 'An unknown server error occurred.'
+    }
+  };
+}
+
+function intercept (err, object) {
+  var e = false;
+
+  if (this.ERRORS[err]) { e = this.ERRORS[err]; }
+  else if (err) { e = this.ERRORS.InternalServerError; }
+  else if (!err && !object) { e = this.ERRORS.NullCollectionError; }
+
+  if (e) {
+    Log.create({ type: 'error', content: JSON.stringify(e) });
+  }
+
+  return e;
+}
