f0fefa68e1295ff4696b2393a38c9d18f6264fe5
Will <will@willricketts.com>
2015-02-10 14:40:14 -0500

    added magellan geocoding service

---
+++ api/services/magellan.js
@@ -0,0 +1,18 @@
+var geotools = require('geo-tools');
+
+module.exports = {
+  geocode: geocode,
+  reverseGeocode: reverseGeocode
+};
+
+function gecode(address, callback) {
+  geocode(address, function(coordinates) {
+    callback(coordinates);
+  });
+}
+
+function reverseGeocode(lat, lng, callback) {
+  reverseGeocode({lat: lat, lng: lng}, function(address){
+    callback(address);
+  });
+}
